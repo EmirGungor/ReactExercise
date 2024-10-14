a49ca09926ff9aed47714ba07b9927ce56c84418
Emir <96845612+EmirGungor@users.noreply.github.com>
2024-10-14 16:55:11 +0300

    Add files via upload

---
+++ src/components/CoreConsept.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable react/prop-types */
+function CoreConcept({ image, title, description }) {
+    return (
+      <>
+        <div>
+          <li>
+            <img src={image} alt={title} />
+            <h3>{title}</h3>
+            <p>{description}</p>
+          </li>
+        </div>
+      </>)
+  }
+
+  export default CoreConcept
